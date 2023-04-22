2056c07ac79885e1456bac7cc01c9ce48489bdc5
Ali <muhammadaliqorbanzadeh@gmail.com>
2023-04-22 17:41:13 +0300

    Add firebase settings

---
+++ src/firebase/errors.ts
@@ -0,0 +1,7 @@
+export const FIREBASE_ERRORS = {
+    "Firebase: Error (auth/email-already-in-use).": "User already exists!",
+    "Firebase: Error (auth/user-not-found).": "Invalid email or password!",
+    "Firebase: Error (auth/wrong-password).": "Invalid email or password!",
+    "Firebase: Error (auth/popup-closed-by-user).": "Something went wrong! Try again later...",
+    "Firebase: Error (auth/account-exists-with-different-credential).": "This email is already associated with other account"
+}
